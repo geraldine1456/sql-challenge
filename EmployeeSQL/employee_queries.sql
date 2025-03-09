26c84bc8cafb00c8a11935ef7c62bdf8a7d81913
geraldine1456 <gcvalencia1456@yahoo.com>
2025-03-09 17:36:51 -0400

    employee queries  update

--- EmployeeSQL/employee_queries.sql
+++ EmployeeSQL/employee_queries.sql
@@ -1,7 +1,8 @@
 -- 1 List the employee number, last name, first name, sex, and salary of each employee.
 SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
 FROM employees e
-LEFT JOIN salaries s ON (e.emp_no = s.emp_no) 
+	LEFT JOIN salaries s 
+		ON (e.emp_no = s.emp_no) 
 ORDER BY e.emp_no;
 
 -- 2 List the first name, last name, and hire date for the employees who were hired in 1986.
